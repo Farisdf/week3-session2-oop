eba51911742a22f19e4e62b56c5f7d15a92bf6a5
Farisdf <farisdfzayyat2004@gmail.com>
2026-08-15 14:56:36 +0300

    Part 1  - Added Person and Student classes

---
+++ university_system.py
@@ -0,0 +1,32 @@
+# ==================== PART 1 - Person and Student ====================
+ 
+class Person:
+    """A person in the university. This is the parent class."""
+ 
+    def __init__(self, person_id, name):
+        self.person_id = person_id
+        self.name = name
+ 
+    def describe(self):
+        """Print a simple description of the person."""
+        print("Person " + str(self.person_id) + ": " + self.name)
+ 
+ 
+# Student IS-A Person, because every student is also a person: a student
+# has an id and a name just like any person, and only adds a major on top.
+# Inheritance makes sense here because we reuse the Person data and
+# behaviour instead of writing it again inside Student.
+class Student(Person):
+    """A student. Inherits everything from Person and adds a major."""
+ 
+    def __init__(self, person_id, name, major):
+        # super() calls the parent __init__ so Person sets up id and name
+        super().__init__(person_id, name)
+        self.major = major
+ 
+    # method overriding: Student has its own version of describe()
+    def describe(self):
+        """Print a description that also shows the major."""
+        print("Student " + str(self.person_id) + ": " + self.name
+              + " - major: " + self.major)
+ 
